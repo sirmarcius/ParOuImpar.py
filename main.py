64328b36ef7625cbb1e675b7cc0d1ed363c5e18c
sirmarcius <>
2021-05-18 00:51:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+# Ver se um número é par ou impar
+P = int(input('Qual o número? '))
+for a in range(0,1):
+  print('Carregando...')
+if P %2:
+  print('O número',P,'é impar!')
+else:
+  print('O número',P,'é par')    
